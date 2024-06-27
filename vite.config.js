f796dc433b75eea6f8784d3631cd695ea0f157bd
Karan Thakur <hipprofessional@Karans-MacBook-Pro.local>
2024-06-27 16:18:26 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-karantha-kur/",
+  base:"/portfolio-karantha-kur/",
   plugins: [react()],
 })
